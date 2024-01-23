0c47e1591aea4bc6018080723b84d65daf27903e
andrewchiu1203 <andrewchiu1203@gmail.com>
2024-01-23 14:54:34 -0500

    timezone/color with django template

--- main/models.py
+++ main/models.py
@@ -2,15 +2,23 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
 from django.utils import timezone
-    
+
+class Color(models.Model):
+    color = models.CharField(max_length = 128)
+
+    def __str__(self):
+        return self.color
+
 class Diary(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE, null = True, blank = True)
     title = models.CharField(max_length = 128)
     body = RichTextField(blank = True, null = True)
     created = models.CharField(default = timezone.now, max_length = 128)
+    order = models.DateTimeField(auto_now_add = True, null = True)
+    color = models.ForeignKey(Color, on_delete = models.CASCADE, null = True)
 
     def __str__(self):
         return self.title
     
     class Meta:
-        ordering = ['-created']
+        ordering = ['-order']
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.urls.base import reverse_lazy
 from .forms import DiaryForm
-from main.models import Diary
+from main.models import Diary, Color
 
 def blank(request):
     return redirect(reverse_lazy("main:index"))
@@ -20,10 +20,18 @@ def index(request):
     else:
         diarys = Diary.objects.filter(user = request.user)
 
+        # If intended to use raw sql query, uncomment this
+        # -------------------------------------------------
+        # id = request.user.id
+        # query = f"SELECT    *               \
+        #           FROM      main_Diary      \
+        #           WHERE     user_id = {id}"
+        # diarys = Diary.objects.raw(query)
+
     return render(request, "main/index.html", {
         "diarys" : diarys,
     })
-    
+
 def signin(request):
     error = False
 
@@ -83,13 +91,23 @@ def update(request, current_id):
     if request.method == "POST":
         new_title = request.POST["title"]
         new_body = request.POST["body"]
+        new_color = request.POST["color"]
 
         Diary.objects.filter(id = current_id, 
                              user = request.user).update(title = new_title)
         Diary.objects.filter(id = current_id, 
                              user = request.user).update(body = new_body)
+        
+        if new_color != "" and new_color is not None:
+            new_color = Color.objects.get(color = new_color)
+            Diary.objects.filter(id = current_id, 
+                                 user = request.user).update(color = new_color)
+        else:
+            Diary.objects.filter(id = current_id, 
+                                 user = request.user).update(color = None)
 
     diary = Diary.objects.get(id = current_id, user = request.user)
+    colors = Color.objects.all()
     form = DiaryForm(instance = diary)
 
     if form.is_valid():
@@ -98,6 +116,7 @@ def update(request, current_id):
     return render(request, "main/update.html", {
         "diary" : diary,
         "form" : form,
+        "colors" : colors,
     })
 
 def create(request):
@@ -107,14 +126,21 @@ def create(request):
     if request.method == "POST":
         new_title = request.POST["title"]
         new_body = request.POST["body"]
+        new_color = request.POST["color"]
 
-        Diary(user = request.user, title = new_title, body = new_body).save()
+        if new_color == "":
+            Diary(user = request.user, title = new_title, body = new_body).save()
+        else:
+            new_color = Color.objects.get(color = new_color)
+            Diary(user = request.user, title = new_title, body = new_body, color = new_color).save()
 
         return redirect(reverse_lazy("main:index"))
 
+    colors = Color.objects.all()
     
     return render(request, "main/create.html", {
         "form" : DiaryForm(),
+        "colors" : colors,
     })
 
 def delete(request, current_id):
